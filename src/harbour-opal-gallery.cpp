05f49e15c19baf8ebedc3564c8d16afb27648d66
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2023-06-03 22:18:32 +0200

    Add sailjail profile

--- src/harbour-opal-gallery.cpp
+++ src/harbour-opal-gallery.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of harbour-opal-gallery.
  * SPDX-License-Identifier: GPL-3.0-or-later
- * SPDX-FileCopyrightText: 2020 Mirian Margiani
+ * SPDX-FileCopyrightText: 2020-2023 Mirian Margiani
  */
 
 #include <QtQuick>
@@ -10,23 +10,15 @@
 
 int main(int argc, char *argv[])
 {
-    // SailfishApp::main() will display "qml/harbour-opal-gallery.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //   - SailfishApp::pathToMainQml() to get a QUrl to the main QML file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
-    //
-    // return SailfishApp::main(argc, argv);
-
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
+    app->setOrganizationName("harbour-opal-gallery"); // needed for Sailjail
+    app->setApplicationName("harbour-opal-gallery");
+
     QScopedPointer<QQuickView> view(SailfishApp::createView());
     view->engine()->addImportPath(SailfishApp::pathTo("qml/modules").toString());
     view->rootContext()->setContextProperty("APP_VERSION", QString(APP_VERSION));
     view->rootContext()->setContextProperty("APP_RELEASE", QString(APP_RELEASE));
+
     view->setSource(SailfishApp::pathToMainQml());
     view->show();
     return app->exec();
