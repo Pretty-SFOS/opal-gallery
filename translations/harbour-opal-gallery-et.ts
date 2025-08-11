fa89b97960bd4981eb2ab8941a6a034222e67124
Priit Jõerüüt <jrthwlate@users.noreply.hosted.weblate.org>
2025-08-11 20:02:16 +0200

    Translated using Weblate (Estonian)
    Currently translated at 88.1% (246 of 279 strings)
    
    Co-authored-by: Priit Jõerüüt <jrthwlate@users.noreply.hosted.weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-gallery/et/
    Translation: Opal for SailfishOS/App: Opal Gallery

--- translations/harbour-opal-gallery-et.ts
+++ translations/harbour-opal-gallery-et.ts
@@ -1134,33 +1134,33 @@
     <message>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="206"/>
         <source>Database upgrade failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Andmebaasi uuendamine ei õnnestunud</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="207"/>
         <source>An error occurred while upgrading the database from version %1 to version %2. Please report this issue.</source>
-        <translation type="unfinished"></translation>
+        <translation>Andmebaasi uuendamine versioonist %1 versiooni %2 ei õnnestunud. Palun teata sellest veast arendajale.</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="213"/>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="238"/>
         <source>Developer information:</source>
-        <translation type="unfinished"></translation>
+        <translation>Teave arendajale:</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="222"/>
         <source>Invalid database version</source>
-        <translation type="unfinished"></translation>
+        <translation>Vigane andmebaasiversioon</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="223"/>
         <source>The app cannot start because the database has version %1 but only version %2 is supported.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kuna andmebaasiversioon on %1, aga toetatud on vaid %2, siis rakenduse käivitamine ei õnnestu.</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="230"/>
         <source>Database Maintenance</source>
-        <translation type="unfinished">Andmebaasi hooldus</translation>
+        <translation>Andmebaasi hooldus</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="231"/>
@@ -1170,12 +1170,12 @@
     <message>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="234"/>
         <source>Database issue</source>
-        <translation type="unfinished"></translation>
+        <translation>Andmebaasi viga</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/LocalStorage/MessageHandler.qml" line="235"/>
         <source>An unexpected issue occurred in the database. Try restarting the app.</source>
-        <translation type="unfinished"></translation>
+        <translation>Andmebaasis tekkis ootamatu viga. Proovi rakendust uuesti käivitada.</translation>
     </message>
 </context>
 <context>
